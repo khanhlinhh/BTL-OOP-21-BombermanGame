f82be4630c296e313b1349713cd2ca41de5057e7
phanhienan <78184196+phanhienan@users.noreply.github.com>
2021-12-09 21:38:11 +0700

    Delete src/uet/oop/bomberman directory

--- src/uet/oop/bomberman/entities/Items/BombItem.java
+++
@@ -1,9 +0,0 @@
-package uet.oop.bomberman.entities.Items;
-
-import javafx.scene.image.Image;
-
-public class BombItem extends Item{
-    public BombItem(int x, int y, Image img) {
-        super(x, y, img);
-    }
-}
--- src/uet/oop/bomberman/entities/Items/SpeedItem.java
+++
@@ -1,9 +0,0 @@
-package uet.oop.bomberman.entities.Items;
-
-import javafx.scene.image.Image;
-
-public class SpeedItem extends Item {
-    public SpeedItem(int x, int y, Image img) {
-        super(x, y, img);
-    }
-}
